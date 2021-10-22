016af9a8c16921bc275d921146e42d1f633e09fe
6kbzn9 <shnbyakuya@gmail.com>
2021-10-22 19:38:55 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "/src/index.css";
+import Container from "@mui/material/Container";
+import Buttons from "./components/Buttons";
+import InputButtons from "./components/InputButtons";
+
+const App = () => (
+  <Container>
+    <Buttons />
+    <InputButtons />
+  </Container>
+);
+
+export default App;
